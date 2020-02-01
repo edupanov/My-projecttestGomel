6abfddbe82708de3df1b30ee05463bbee460cd19
edupanov <edupanov@gmail.com>
2020-02-01 16:07:12 +0300

    правки
    поправил верстку

---
+++ js/main.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    'use strict';
+
+    const visa = document.getElementById('visa');
+    const mastercard = document.getElementById('mastercard');
+    const maestro = document.getElementById('maestro');
+
+
+});
